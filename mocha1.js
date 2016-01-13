ce5ce593e6f83f87435b90ef198b2da97f760ff7
srivathsankr7 <srivathsankr7@gmail.com>
2016-01-13 22:16:34 +0530

    Parser program

---
+++ mocha1.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe('suite',function() {
+	it('test1', function() {
+		assert.equal('a','a');
+	});
+});
---
+++ parsing.js
@@ -0,0 +1,4 @@
+var s = "123";
+console.log(parseInt(s) + 100);
+var sf = "12.121";
+console.log(parseFloat(sf) + 11);
